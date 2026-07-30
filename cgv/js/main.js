acd8d2b134fe24d802b936f26c97fd60977584fe
biidayy <huy.2374802010778@vanlanguni.vn>
2026-07-30 12:39:50 +0700

    Remove incomplete Khai auth pages and restore Huy site navigation.

--- cgv/js/main.js
+++ cgv/js/main.js
@@ -20,8 +20,7 @@ function renderHeader(active) {
       <a href="#">TIN MỚI & ƯU ĐÃI</a>
       <a href="ve-cua-toi.html">VÉ CỦA TÔI${ticketCount ? ` (${ticketCount})` : ""}</a>
       <a href="ve-cua-toi.html#favorites">YÊU THÍCH${favCount ? ` (${favCount})` : ""}</a>
-      <a href="hosocanhan.html">HỒ SƠ</a>
-      <a href="dangnhap.html">ĐĂNG NHẬP/ ĐĂNG KÝ</a>
+      <a href="#">ĐĂNG NHẬP/ ĐĂNG KÝ</a>
       <div class="lang-switch"><span class="active">VN</span><span>EN</span></div>
     </div>
   </div>
